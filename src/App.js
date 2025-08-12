31fdbf5632122ea65396acbacc49615f19052b6c
KristineNeilStudio <71912447+KristineNeilStudio@users.noreply.github.com>
2025-08-12 23:19:30 +0000

    Set up

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import UXGapFinder from './UXGapFinder';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <UXGapFinder />
+    </div>
+  );
+}
+
+export default App;
